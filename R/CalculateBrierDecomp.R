3db64662ccaca0ab0fd02cc8cf2dd33888da3887
Stefan Siegert <stefan_siegert@gmx.de>
2013-03-25 00:00:00 +0000

    version 1.2

--- R/CalculateBrierDecomp.R
+++ R/CalculateBrierDecomp.R
@@ -1,162 +1,185 @@
 #the main function
 CalculateBrierDecomp <- function(p, y, n.bins) {
 
-  #initialize main object to return
-  bride.list <- list()
-  bride.list$p <- p
-  bride.list$y <- y
+  n <- length(p)
 
-  #p bins
-  p.breaks <- seq(0, 1, length.out = n.bins + 1) + 
-               c(-0.01, rep(0, n.bins - 1), 0.01)
-  ##get bin no. of each p-value
+  #p.binning (vector of length n with entries equal to bin-no. of p_n)
+  p.breaks <- seq(0, 1, length.out = n.bins + 1)
   p.binning <- cut(p, breaks=p.breaks, include.lowest=TRUE, ordered_result=TRUE)
   p.binning <- as.numeric(p.binning)
-  # calculate the in-bin averages
-  p.means <- lapply( seq(n.bins),
-               function(z){ 
-                 mean(p[which(p.binning == z)]) 
-               }
-             ) 
-  p.means <- unlist(p.means)
-  # replace p-values by in-bin averages
-  p <- p.means[as.vector(unlist(p.binning))]
 
-  #store
-  bride.list$n.bins <- n.bins
-  bride.list$p.binavgs <- p.means
-  bride.list$p.binned <- p
+  ##construct matrices and column sums
+  m.ind <- matrix(c(seq(n), p.binning), nrow=n)
+  m.A <- matrix(0, nrow=n, ncol=n.bins)
+  m.A[m.ind] <- 1
+  cs.A <- colSums(m.A)
+  m.B <- matrix(0, nrow=n, ncol=n.bins)
+  m.B[m.ind] <- y
+  cs.B <- colSums(m.B)
+  m.C <- matrix(0, nrow=n, ncol=n.bins)
+  m.C[m.ind] <- p
+  cs.C <- colSums(m.C)
+  m.Y <- matrix(y, ncol=1)
+  cs.Y <- colSums(m.Y)
 
-
-  ##calculate cross table
-  # p-bins
-  c.names <- paste(seq(n.bins))
-  # y-classes
-  r.names <- paste(sort(unique(y)))
-  # expand grid for each entry of cross table
-  ctab.grid <- as.matrix(expand.grid(r.names, c.names))
-  # calculate individual elements of cross table
-  ctab.grid <- apply(ctab.grid, 1, 
-                 function(z) { 
-                   length( intersect( which( p.binning == z[2] ), 
-                                      which( y == z[1] ) )) 
-                 } 
-               )
-  c.table <- matrix(ctab.grid, ncol=n.bins)
-  colnames(c.table) <- c.names
-  rownames(c.table) <- r.names
-
-  #store
-  bride.list$c.table <- c.table
-
-  n.od <- colSums(c.table)
-  n <- sum(c.table)
-
-
-  #biased decomposition
-  inds <- which(n.od > 0)
-  rel <- sum((n.od / n * (p.means - c.table["1", ] / n.od)^2)[inds])
-  res <- sum((n.od / n * 
-              (c.table["1", ] / n.od - sum(c.table["1", ]) / n)^2)[inds] )
-  unc <- sum(c.table["0", inds]) * sum(c.table["1", inds]) / n^2
-
-  #store
-  bride.list$rel <- rel
-  bride.list$res <- res
-  bride.list$unc <- unc
-  bride.list$br <- rel - res + unc
-
-  #bias-corrected decomposition
-  inds <- which(n.od > 1) #avoid division by zero
-  r1 <- -1 * sum(((c.table["0", ] * c.table["1", ]) / 
-        (n.od * (n.od - 1)))[inds] ) / n
-  r2 <- sum(c.table["0", inds]) * sum(c.table["1", inds]) / 
-        (n^2 * (n - 1))
-  rel2a <- rel + r1
-  res2a <- res + r1 + r2
-  unc2 <- unc + r2
-
-  rel2 <- max(c(rel2a, rel2a - res2a, 0))
-  res2 <- max(c(res2a, res2a - rel2a, 0))
-
-  #store
-  bride.list$rel2 <- rel2
-  bride.list$res2 <- res2
-  bride.list$unc2 <- unc2
-
-  #variances of reliability, resolution, uncertainty
-  vrel <- 0.0
-  vres <- 0.0
-  vrelunb <- 0.0
-  vresunb <- 0.0
-  I4 <- y
-  v4 <- var(I4)
-  Fd <- sum(I4)
-  dFresunb <- (n - 2 * Fd) / n^3 / (n - 1)
-
-  for (d in seq(n.bins)) {
-    I1 <- 1 * (p.binning == d)
-    if (sum(I1) > 1) {
-      I2 <- I1 * y
-      I3 <- I1 * p
-      v1 <- var(I1)
-      v2 <- var(I2)
-      v3 <- var(I3)
-      c12 <- cov(I1, I2)
-      c23 <- cov(I2, I3)
-      c13 <- cov(I1, I3)
-      c14 <- cov(I1, I4)
-      c24 <- cov(I2, I4)
-  
-      Ad <- sum(I1)
-      Bd <- sum(I2)
-      Cd <- sum(I3)
-  
-      f <- Bd - Cd
-      vrel <- vrel + (f / Ad)^4 * v1 + (2.0 * f / Ad)^2 * (v2 + v3) - 
-                       4.0 * (f / Ad)^3 * (c12 - c13) - 
-                       8.0 * (f / Ad)^2 * c23
-  
-      f1 <- Bd / Ad - Fd / n
-      f2 <- Bd / Ad + Fd / n
-      vres <- vres + f1 * f1 * f2 * f2 * v1 + 4.0 * f1 * f1 * v2 - 
-                       4.0 * f1 * f1 * f2 * c12 
-  
-      dArelunb <- -1.0 / (n * Ad^2) * 
-                  ((Bd - Cd)^2 - Ad * Bd / (Ad - 1) - 
-                   Bd * (Bd - Ad) / ((Ad - 1)^2))
-      dBrelunb <- (2 * Bd - 1) / (Ad - 1) / n - 2 * Cd / n / Ad
-      dCrelunb <- -1. / (n * Ad) * 2 * (Bd - Cd)
-      vrelunb <- vrelunb + n * (dArelunb^2 * v1 + 
-                                dBrelunb^2 * v2 +
-                                dCrelunb^2 * v3 +
-                                2 * dArelunb * dBrelunb * c12 +
-                                2 * dArelunb * dCrelunb * c13 +
-                                2 * dBrelunb * dCrelunb * c23 )
-      dAresunb <- -f1 * f2 / n +
-                   Bd / n / Ad^2 / (Ad-1)^2 * ((Ad-Bd)^2 - Bd * (Bd-1))
-      dBresunb <- 2 * f1 / n - (Ad - 2 * Bd) / Ad / n / (Ad - 1)
-      vresunb <- vresunb + n * (dAresunb^2 * v1 +
-                                  dBresunb^2 * v2 +
-                                  2 * dAresunb * dBresunb * c12 +
-                                  2 *dAresunb * dFresunb * c14 +
-                                  2 * dBresunb * dFresunb * c24)
+  ## sets of indices for which colSums(A) > 0, resp. > 1
+  ## in order to avoid division by zero
+  d0 <- which(cs.A < 1)
+  d1 <- which(cs.A < 2)
+  #function that returns x_i : i \in d0
+  d0.set <- function(x) {
+    if (length(d0) > 0) {
+      x <- x[-d0]
     }
-
+    x
   }
-  vresunb <- vresunb + n * (dFresunb^2 * v4)
-  vunc <- (1 - 2 * Fd / n)^2 / n * v4
-  vuncunb <- (n - 2 * Fd)^2 / n / (n - 1)^2 * v4
-  vrel <- vrel / n
-  vres <- vres / n
+  #function that returns x_i : i \in d1
+  d1.set <- function(x) {
+    if (length(d1) > 0) {
+      x <- x[-d1]
+    }
+    x
+  }
 
-  #store
-  bride.list$relv <- vrel
-  bride.list$resv <- vres
-  bride.list$uncv <- vunc
-  bride.list$rel2v <- vrelunb
-  bride.list$res2v <- vresunb
-  bride.list$unc2v <- vuncunb
+  ## calculate estimators
+  rel <- 1/n * sum( d0.set((cs.B - cs.C)^2 / cs.A) )
+  res <- 1/n * sum( d0.set(cs.A * (cs.B / cs.A - cs.Y / n)^2) )
+  unc <- cs.Y * (n - cs.Y) / (n * n)
+
+  ## correction factors for bias removal
+  corr.s <- 1 / n * sum(d1.set(cs.B * (cs.A - cs.B) / (cs.A * (cs.A - 1))))
+  corr.t <- cs.Y * (n - cs.Y) / n / n / (n-1)
+
+  # avoid rel<0, res<0, res>1, and unc>1/4
+  alpha <- min(c(rel/corr.s, 
+                 max(c(res / (corr.s - corr.t), 
+                       (res - 1) / (corr.s - corr.t))),
+                 (1 - 4 * unc) / (4 * corr.t),
+                 1)
+              )
+
+  rel2 <- rel - alpha * corr.s
+  res2 <- res - alpha * corr.s + alpha * corr.t
+  unc2 <- unc + alpha * corr.t
+                  
+
+
+  ###################################################
+  # variances of reliability, resolution, uncertainty
+  # estimated by propagation of uncertainty
+  ###################################################
+
+  m.cent <- diag(n) - 1 / n
+
+  #####
+  # REL
+  #####
+  d.rel.d.a <- -1 * (cs.B - cs.C) * (cs.B - cs.C) / (n * cs.A * cs.A)
+  d.rel.d.b <- 2 * (cs.B - cs.C) / (n * cs.A)
+  d.rel.d.c <- -2 * (cs.B - cs.C) / (n * cs.A)
+  if (length(d0) > 0) {
+    d.rel.d.a[d0] <- 0
+    d.rel.d.b[d0] <- 0
+    d.rel.d.c[d0] <- 0
+  }
+  jacobian.rel <- matrix(c(d.rel.d.a, d.rel.d.b, d.rel.d.c), nrow=1)
+
+  m.X <- cbind(m.A, m.B, m.C)
+  cov.X.rel <- t(m.X) %*% m.cent %*% m.X
+
+  var.rel <- drop( jacobian.rel %*% cov.X.rel %*% t(jacobian.rel) )
+
+  #####
+  # RES
+  #####
+  d.res.d.a <- -1 / n * (cs.B / cs.A - cs.Y / n) * 
+                      (cs.B / cs.A + cs.Y / n)
+  d.res.d.b <- 2 / n * (cs.B / cs.A - cs.Y / n)
+  d.res.d.y <- 0
+  if (length(d0) > 0) {
+    d.res.d.a[d0] <- 0
+    d.res.d.b[d0] <- 0
+  }
+  jacobian.res <- matrix(c(d.res.d.a, d.res.d.b, d.res.d.y), nrow=1)
+
+  m.X <- cbind(m.A, m.B, m.Y)
+  cov.X.res <- t(m.X) %*% m.cent %*% m.X
+  
+  var.res <- drop( jacobian.res %*% cov.X.res %*% t(jacobian.res) )
+
+  #####
+  # UNC
+  #####
+  var.unc <- drop((1 - 2 * cs.Y / n) * (1 - 2 * cs.Y / n) / n / n * 
+                   t(m.Y) %*% m.cent %*% m.Y)
+
+
+  ######
+  # REL'
+  ######
+  d.rel2.d.a <- -1 * ((cs.B - cs.C) * (cs.B - cs.C) - 
+                      (cs.A * cs.B) / (cs.A - 1) - 
+                      cs.B * (cs.B - cs.A) / (cs.A - 1) / (cs.A - 1)
+                     ) / (n * cs.A * cs.A)
+  d.rel2.d.b <- (2 * cs.B - 1) / (n * cs.A - n) - 2 * cs.C / (n * cs.A)
+  d.rel2.d.c <- -2 * (cs.B - cs.C) / (n * cs.A)
+  if (length(d1) > 0) {
+    d.rel2.d.a[d1] <- 0
+    d.rel2.d.b[d1] <- 0
+    d.rel2.d.c[d1] <- 0
+  }
+  jacobian.rel <- matrix(c(d.rel2.d.a, d.rel2.d.b, d.rel2.d.c), nrow=1)
+
+  var.rel2 <- drop( jacobian.rel %*% cov.X.rel %*% t(jacobian.rel) )
+
+
+  ######
+  # RES'
+  ######
+
+  d.res2.d.a <- -1 / n * (cs.B / cs.A - cs.Y / n) * (cs.B / cs.A + cs.Y / n) +
+                 cs.B / (n * cs.A * cs.A * (cs.A - 1) * (cs.A - 1)) *
+                 ((cs.A - cs.B) * (cs.A - cs.B) - cs.B * (cs.B - 1))
+  d.res2.d.b <- 2 / n * (cs.B / cs.A - cs.Y / n) - (cs.A - 2 * cs.B) /
+                (n * cs.A * (cs.A - 1))
+  d.res2.d.y <- (n - 2 * cs.Y) / n / n / n / (n - 1)
+  if (length(d1) > 0) {
+    d.res2.d.a[d1] <- 0
+    d.res2.d.b[d1] <- 0
+  }
+
+  jacobian.res2 <- matrix(c(d.res2.d.a, d.res2.d.b, d.res2.d.y), nrow=1)
+
+  var.res2 <- drop(jacobian.res2 %*% cov.X.res %*% t(jacobian.res2))
+
+  ######
+  # UNC'
+  ######
+ 
+  var.unc2 <- drop((n - 2 * cs.Y) * (n - 2 * cs.Y) / n / n / (n - 1) / (n - 1) *
+                   t(m.Y) %*% m.cent %*% m.Y)
+  
+
+
+  #store everything
+  bride.list <- list(
+                  p = p,
+                  y = y,
+                  n.bins = n.bins,
+                  rel = rel,
+                  res = res,
+                  unc = unc,
+                  rel2 = rel2,
+                  res2 = res2,
+                  unc2 = unc2,
+                  br = rel - res + unc,
+                  rel.var = var.rel,
+                  res.var = var.res,
+                  unc.var = var.unc,
+                  rel2.var = var.rel2,
+                  res2.var = var.res2,
+                  unc2.var = var.unc2
+                )
 
   #return
   bride.list 
